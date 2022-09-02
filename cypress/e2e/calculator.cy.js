f21165459a845327b09fec50b7e5cde32932bf1e
Andrew Patterson <104862968+zlatterson@users.noreply.github.com>
2022-09-02 13:09:07 +0100

    Update calculator.cy.js

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -71,6 +71,7 @@ describe("Calculator", () => {
     cy.get('#number9').click();
     cy.get('#operator-divide').click();
     cy.get('#number0').click();
+    cy.get('#number0').click();
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', 'Infinity');
   })
